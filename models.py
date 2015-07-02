f1c11435867f0e69050d0ccbadc34b940cb585b3
Zachary Goodman <zjg95@cs.utexas.edu>
2015-07-02 16:39:00 -0500

    updated models file

--- models.py
+++ models.py
@@ -1,1 +1,19 @@
-print("hello world")
+from django.db import models
+
+class Family(models.Model):
+	name = models.CharField(max_length=50)
+
+	def __str__(self):
+		return "House " + self.name
+
+class Person(models.Model):
+    first_name  = models.CharField(max_length=50)
+    family_name = models.CharField(max_length=50)
+    castle_name = models.CharField(max_length=50)
+
+    def __str__(self):
+    	return self.first_name + " " + self.family_name
+
+class Castle(models.Model):
+    ruling_family = models.ForeignKey(Family)
+    name = models.CharField(max_length=100)
