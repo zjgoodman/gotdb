065dba20724240dbbb0ad8c22c5144a3c1a8c9ff
Michael Gutierrez <sdragoon@cs.utexas.edu>
2015-07-03 16:01:30 -0500

    places now have info, still need a bit more parsing

--- soupscraper.py
+++ soupscraper.py
@@ -16,11 +16,13 @@ from bs4 import BeautifulSoup
 
 # for house in houses:
 # 	if not house.get():
-# 		
+#
+hdr = {'User-Agent': 'Mozilla/5.0'}
+
+print("People:\n")
 for name in ('Eddard_Stark', 'Tywin_Lannister', 'Robert_Baratheon'):
 	# name = 'Eddard_Stark'
 	site = 'http://www.westeros.org/GoT/Characters/Entry/' + name
-	hdr = {'User-Agent': 'Mozilla/5.0'}
 	req = urllib2.Request(site, headers=hdr)
 	page = urllib2.urlopen(req)
 	soup = BeautifulSoup(page)
@@ -38,4 +40,37 @@ for name in ('Eddard_Stark', 'Tywin_Lannister', 'Robert_Baratheon'):
 		parag = parag.replace("<em>", "")
 		parag = parag.replace("</em>", "")
 		print(parag[3:-4])
-	print('\n')
+	print('\n\n')
+
+print("Places:\n")
+for place in ('Winterfell', 'Storm\'s_End', 'Casterly_Rock'):
+	#place = 'Winterfell'
+	site = 'http://gameofthrones.wikia.com/wiki/' + place
+	page = urllib2.urlopen(site)
+	soup = BeautifulSoup(page)
+	print(place + ':')
+	data = soup.find('div', id = "mw-content-text")
+	for parag in data.find_all('p'):
+		parag = str(parag)
+		while parag.find('<') != -1 and parag.find('>') != -1:
+			start = parag.find('<')
+			cut = start
+			end = parag.find('>')
+
+			while (start < end):
+				start += 1
+				if parag[start] == '<':
+					cut = start
+			parag = parag[:cut] + parag[end + 1:]
+		while parag.find('[') != -1 and parag.find(']') != -1:
+			start = parag.find('[')
+			cut = start
+			end = parag.find(']')
+
+			while (start < end):
+				start += 1
+				if parag[start] == '[':
+					cut = start
+			parag = parag[:cut] + parag[end + 1:]
+		print(parag)
+	print('\n\n')
