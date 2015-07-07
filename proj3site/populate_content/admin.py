137592f181aa097371cc02df45673cb75629af86
Zach Goodman <zjg95@wireless-10-147-70-41.public.utexas.edu>
2015-07-07 14:05:46 -0500

    added house model

--- proj3site/populate_content/admin.py
+++ proj3site/populate_content/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
-from .models import Person, Region, Castle
+from .models import Person, Region, Castle, House
 
 admin.site.register(Person)
 admin.site.register(Region)
 admin.site.register(Castle)
-#admin.site.register(House)
+admin.site.register(House)
