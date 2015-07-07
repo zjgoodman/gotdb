6f919452f9a5f94115d5bdefcf7b9c39d5de5eea
Zach Goodman <zjg95@wireless-10-145-247-174.public.utexas.edu>
2015-07-06 20:13:55 -0500

    added more to the models

--- proj3site/populate_content/models.py
+++ proj3site/populate_content/models.py
@@ -4,6 +4,7 @@ class Person(models.Model) :
     # house = models.ForeignKey(House)
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
+    house_name = models.ForeignKey('House', null=True)
     titles = models.CharField(max_length=200)
     status = models.CharField(max_length=200)
     bio = models.TextField() 
@@ -13,14 +14,27 @@ class Person(models.Model) :
 
 class Region(models.Model) :
     name = models.CharField(max_length=200)
-    # capital = models.ForeignKey(Castle)
+    # ruling_house = models.ForeignKey('House', null=True)
+    # capital = models.ForeignKey('Castle', null=True)
+    ruling_lord = models.ForeignKey(Person, null=True)
     description = models.TextField()
     def __str__(self):
         return self.name
 
 class Castle(models.Model) :
     name = models.CharField(max_length=200)
-    region = models.ForeignKey(Region)
+    region = models.ForeignKey(Region, null=True)
+    description = models.TextField(null=True)
+    ruling_lord = models.ForeignKey(Person, null=True)
+    # ruling_house = models.ForeignKey('House', null=True)
+    def __str__(self):
+        return self.name
+
+class House(models.Model) :
+    name = models.CharField(max_length=200)
+    region = models.ForeignKey(Region, null=True)
+    castle = models.ForeignKey(Castle, null=True)
+    description = models.TextField(null=True)
     def __str__(self):
         return self.name
     
