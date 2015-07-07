58e84210aa523cb416ce8182287030b17257806b
Scott Stephens <stevo4932@utexas.edu>
2015-07-07 13:27:52 -0500

    added api

--- proj3site/populate_content/urls.py
+++ proj3site/populate_content/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
     url(r'^people/(?P<person_name>\w+_\w+)/$', views.person_detail, name='person_detail'),
     url(r'^regions/(?P<region_name>[a-zA-z ]+)/$', views.region_detail, name='region_detail'),
     url(r'^castles/(?P<castle_name>[\'a-zA-z ]+)/$', views.castle_detail, name='castle_detail'),
+    url(r'^api/people/$', views.person_api_list),
+    url(r'^api/castles/$', views.castle_api_list),
+    url(r'^api/regions/$', views.region_api_list),
 ]
 
--- proj3site/populate_content/views.py
+++ proj3site/populate_content/views.py
@@ -3,6 +3,10 @@ import string
 from django.http import HttpResponse, Http404
 from django.shortcuts import render
 from django.template import RequestContext, loader
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from .models import Castle, Region, Person
+from .serializers import PeopleSerializer, RegionSerializer, CastleSerializer
 
 from .models import Person, Region, Castle
 
@@ -45,3 +49,39 @@ def castle_detail(request, castle_name):
     except Castle.DoesNotExist:
         raise Http404("Castle does not exist :")
     return render(request, 'populate_content/castle_detail.html', {'castle': castle})
+
+class JSONResponse(HttpResponse):
+    """
+    An HttpResponse that renders its content into JSON.
+    """
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
+
+def person_api_list(request):
+    """
+    List all code snippets, or create a new snippet.
+    """
+    if request.method == 'GET':
+        people = Person.objects.all()
+        serializer = PeopleSerializer(people, many=True)
+        return JSONResponse(serializer.data)
+
+def region_api_list(request):
+    """
+    List all code snippets, or create a new snippet.
+    """
+    if request.method == 'GET':
+        region = Region.objects.all()
+        serializer = RegionSerializer(region, many=True)
+        return JSONResponse(serializer.data)
+
+def castle_api_list(request):
+    """
+    List all code snippets, or create a new snippet.
+    """
+    if request.method == 'GET':
+        castle = Castle.objects.all()
+        serializer = CastleSerializer(castle, many=True)
+        return JSONResponse(serializer.data)
