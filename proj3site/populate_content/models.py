7e38bb6eca4d9a57656c807475532549bdca1e48
Zach Goodman <zjg95@wireless-10-147-70-41.public.utexas.edu>
2015-07-07 14:25:55 -0500

    updated database to include model id values. this will help with finding the data

--- proj3site/populate_content/models.py
+++ proj3site/populate_content/models.py
@@ -13,6 +13,7 @@ class Person(models.Model) :
     - Bio
     """
     # name info
+    person_id = models.CharField(max_length=200, null=True)
     first_name = models.CharField(max_length=200, null=True)
     last_name = models.CharField(max_length=200, null=True)
     house_name = models.ForeignKey('House', null=True)
@@ -43,7 +44,8 @@ class Region(models.Model) :
     - History 
     """
     # name
-    name = models.CharField(max_length=200)
+    region_id = models.CharField(max_length=200, null=True)
+    name = models.CharField(max_length=200, null=True)
     
     # capital
     capital_name = models.ForeignKey('Castle', null=True)
@@ -71,6 +73,7 @@ class Castle(models.Model) :
     - History
     """
     # name and region
+    castle_id = models.CharField(max_length=200, null=True)
     name = models.CharField(max_length=200, null=True)
     region_name = models.ForeignKey(Region, null=True)
 
@@ -88,23 +91,24 @@ class Castle(models.Model) :
         ordering = ('name',)
 
 class House(models.Model) :
-   # name
-   name = models.CharField(max_length=200, null=True)
+    # name
+    house_id = models.CharField(max_length=200, null=True)
+    name = models.CharField(max_length=200, null=True)
 
-   # famous expressions
-   words = models.CharField(max_length=300, null=True)
+    # famous expressions
+    words = models.CharField(max_length=300, null=True)
 
-   # region of the world
-   region_name = models.ForeignKey(Region, null=True)
+    # region of the world
+    region_name = models.ForeignKey(Region, null=True)
 
-   # the name of their castle
-   castle_name = models.ForeignKey(Castle, null=True)
+    # the name of their castle
+    castle_name = models.ForeignKey(Castle, null=True)
 
-   # brief description
-   description = models.TextField(null=True)
+    # brief description
+    description = models.TextField(null=True)
 
-   def __str__(self):
-       return self.name
+    def __str__(self):
+        return self.name
 
-   class Meta:
-       ordering = ('name',)
+    class Meta:
+        ordering = ('name',)
