3d249743edddd0453db8ab8bb6064ecf19f223ae
Zach Goodman <zjg95@wireless-10-147-70-41.public.utexas.edu>
2015-07-09 16:53:39 -0500

    updated tests.py's url. #56

--- proj3site/populate_content/tests.py
+++ proj3site/populate_content/tests.py
@@ -257,8 +257,8 @@ class GOT_Tests (TestCase):
 
 class GOT_API_Tests (TestCase):
 
-    global_url = "http://104.130.27.102:8000"
-    local_url  = "http://127.0.0.1:8000"
+    # url = "http://104.130.27.102:8000" # rackspace
+    url  = "http://127.0.0.1:8000"     # local
 
     def test_api_get_person (self):
         """
@@ -266,7 +266,7 @@ class GOT_API_Tests (TestCase):
         Expecting a response with a json object
         containing a collection each persons data members
         """
-        response = requests.get(self.local_url + '/content/api/people/')
+        response = requests.get(self.url + '/content/api/people/')
         self.assertEqual(response.status_code, 200)
         actual = response.json()  # convert response to json obj
         # expected = [{"id":2,"first_name":"Robert","last_name":"Baratheon","status":"Dead","bio":"He loves women"},{"id":3,"first_name":"Tywin","last_name":"Lannister","status":"Dead","bio":"Tywin is (was) pretty scary"},{"id":1,"first_name":"Ned","last_name":"Stark","status":"Dead","bio":"Ned is cool"}]
@@ -280,7 +280,7 @@ class GOT_API_Tests (TestCase):
         Expecting a response with a json object
         containing a collection each castle data members
         """
-        response = requests.get(self.local_url + '/content/api/castles/')
+        response = requests.get(self.url + '/content/api/castles/')
         self.assertEqual(response.status_code, 200)
         actual = response.json()  # convert response to json obj
         # expected = [{"id":2,"name":"Casterly Rock","region_name":2,"ruling_lord":3,"description":"The seat of house Lannister"},{"id":3,"name":"Storm's End","region_name":3,"ruling_lord":2,"description":"The seat of house Baratheon"},{"id":1,"name":"Winterfell","region_name":1,"ruling_lord":1,"description":"The seat of house Stark"}]
@@ -294,7 +294,7 @@ class GOT_API_Tests (TestCase):
         Expecting a response with a json object
         containing a collection each regions data members
         """
-        response = requests.get(self.local_url + '/content/api/regions/')
+        response = requests.get(self.url + '/content/api/regions/')
         self.assertEqual(response.status_code, 200)
         actual = response.json()  # convert response to json obj
         # expected = [{"id":3,"name":"Stormlands","capital_name":3,"ruling_lord":2,"description":"The Baratheon lands."},{"id":1,"name":"The North","capital_name":1,"ruling_lord":1,"description":"A big fancy place"},{"id":2,"name":"Westerlands","capital_name":2,"ruling_lord":3,"description":"The Lannister lands"}]
