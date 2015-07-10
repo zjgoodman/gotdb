7975a1d6406148e880d09a2cfd05f1c367c3f6b7
Michael Gutierrez <sdragoon@cs.utexas.edu>
2015-07-10 13:37:19 -0500

    beginning formatting, making separate people file for find/replace

--- soupscraper.py
+++ soupscraper.py
@@ -39,31 +39,6 @@ for name in ('Eddard_Stark', 'Tywin_Lannister', 'Robert_Baratheon', 'Daenerys_Ta
 	# print(data.find('div', attrs={'class': 'CharActor'})[10:-4])
 	for parag in data.find_all('p'):
 		parag = str(parag)
-		parag = parag.replace("<em>", "")
-		parag = parag.replace("</em>", "")
-		print(parag[3:-4]) #removes html brackets
-	print('\n\n')
-
-#using gameofthrones.wikia.com
-#finds and scrapes info on all places and regions listed in string, places first
-print("\nPlaces:\n")
-regions = False
-houses = False
-for place in ('Winterfell', 'Storm\'s_End', 'Casterly_Rock', 'Dragonstone', 'Pyke_(castle)', 'The_Dreadfort', 'King\'s_Landing', 'Highgarden', 'Asshai', 'Tyrosh', 'Bear_Island', 'The_Fingers', 'Clegane\'s_Keep', 'Braavos', 'Vaes_Dothrak', 'Evenfall_Hall', 'Riverrun', 'Lys', 'Lorath', 'Volantis', 'Hellholt', 'Horn_Hill', 'Craster\'s_Keep', 'The_North', 'The_Stormlands', 'The_Westerlands', 'Iron_Islands', 'The_Crownlands', 'The_Reach', 'Essos', 'The_Vale_of_Arryn', 'Beyond_the_Wall', 'The_Riverlands', 'Dorne', 'House_Stark', 'House_Baratheon', 'House_Lannister', 'House_Targaryen', 'House_Martell', 'House_Bolton', 'House_Tyrell', 'House_Mormont', 'House_Baelish', 'House_Clegane'):
-	#place = 'Winterfell'
-	if place == 'The_North':
-		print("\nRegions:\n")
-		regions = True
-	if place == 'House_Stark':
-		print("\nHouses:\n")
-		houses = True
-	site = 'http://gameofthrones.wikia.com/wiki/' + place
-	page = urllib2.urlopen(site)
-	soup = BeautifulSoup(page)
-	print(place + ':')
-	data = soup.find('div', id = "mw-content-text")
-	for parag in data.find_all('p'):
-		parag = str(parag)
 		#Parse out any '<' or '>' and inbetween to remove ugly html
 		while parag.find('<') != -1 and parag.find('>') != -1:
 			start = parag.find('<')
@@ -75,18 +50,55 @@ for place in ('Winterfell', 'Storm\'s_End', 'Casterly_Rock', 'Dragonstone', 'Pyk
 				if parag[start] == '<':
 					cut = start
 			parag = parag[:cut] + parag[end + 1:] #removed bracketed section
-		#Parse out any brackets and references inbetween
-		while parag.find('[') != -1 and parag.find(']') != -1:
-			start = parag.find('[')
-			cut = start
-			end = parag.find(']')
+		print("<p>" + parag + "</p>")
 
-			while (start < end):
-				start += 1
-				if parag[start] == '[':
-					cut = start
-			parag = parag[:cut] + parag[end + 1:] #removed bracketed section
-		#take out sentence long paragraphs or junk
-		if parag.count(".") > 1:
-			print("<p>" + parag + "</p>")
 	print('\n\n')
+
+#using gameofthrones.wikia.com
+#finds and scrapes info on all places and regions listed in string, places first
+# print("\nPlaces:\n")
+# regions = False
+# houses = False
+# for place in ('Winterfell', 'Storm\'s_End', 'Casterly_Rock', 'Dragonstone', 'Pyke_(castle)', 'The_Dreadfort', 'King\'s_Landing', 'Highgarden', 'Asshai', 'Tyrosh', 'Bear_Island', 'The_Fingers', 'Clegane\'s_Keep', 'Braavos', 'Vaes_Dothrak', 'Evenfall_Hall', 'Riverrun', 'Lys', 'Lorath', 'Volantis', 'Hellholt', 'Horn_Hill', 'Craster\'s_Keep', 'The_North', 'The_Stormlands', 'The_Westerlands', 'Iron_Islands', 'The_Crownlands', 'The_Reach', 'Essos', 'The_Vale_of_Arryn', 'Beyond_the_Wall', 'The_Riverlands', 'Dorne', 'House_Stark', 'House_Baratheon', 'House_Lannister', 'House_Targaryen', 'House_Martell', 'House_Bolton', 'House_Tyrell', 'House_Mormont', 'House_Baelish', 'House_Clegane'):
+# 	#place = 'Winterfell'
+# 	if place == 'The_North':
+# 		print("\nRegions:\n")
+# 		regions = True
+# 	if place == 'House_Stark':
+# 		print("\nHouses:\n")
+# 		houses = True
+# 	site = 'http://gameofthrones.wikia.com/wiki/' + place
+# 	page = urllib2.urlopen(site)
+# 	soup = BeautifulSoup(page)
+# 	print(place + ':')
+# 	data = soup.find('div', id = "mw-content-text")
+# 	for parag in data.find_all('p'):
+# 		parag = str(parag)
+# 		#Parse out any '<' or '>' and inbetween to remove ugly html
+# 		while parag.find('<') != -1 and parag.find('>') != -1:
+# 			start = parag.find('<')
+# 			cut = start
+# 			end = parag.find('>')
+
+# 			while (start < end):
+# 				start += 1
+# 				if parag[start] == '<':
+# 					cut = start
+# 			parag = parag[:cut] + parag[end + 1:] #removed bracketed section
+# 		#Parse out any brackets and references inbetween
+# 		while parag.find('[') != -1 and parag.find(']') != -1:
+# 			start = parag.find('[')
+# 			cut = start
+# 			end = parag.find(']')
+
+# 			while (start < end):
+# 				start += 1
+# 				if parag[start] == '[':
+# 					cut = start
+# 			parag = parag[:cut] + parag[end + 1:] #removed bracketed section
+# 		parag = parag.replace("\"", "\\\"") #add escape sequences
+# 		parag = parag.replace("\'", "\\\'")
+# 		#take out sentence long paragraphs or junk
+# 		if parag.count(".") > 1:
+# 			print("<p>" + parag + "</p>")
+# 	print('\n\n')
