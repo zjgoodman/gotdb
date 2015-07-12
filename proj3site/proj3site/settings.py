00ee83b3dc2da40d970168aa6c3c28a612b11f8d
mhauglund <mhauglund@utexas.edu>
2015-07-12 16:15:35 -0500

    working on house detail page

--- proj3site/proj3site/settings.py
+++ proj3site/proj3site/settings.py
@@ -74,14 +74,18 @@ WSGI_APPLICATION = 'proj3site.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'game_of_thrones',
+#        'USER': 'zjg95',
+#        'PASSWORD': 'downing11',
+#        'HOST': '104.130.27.102',
+#        'PORT': '3306',
+#    }
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'game_of_thrones',
-        'USER': 'zjg95',
-        'PASSWORD': 'downing11',
-        'HOST': '104.130.27.102',
-        'PORT': '3306',
-    }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
 }
 
 # Internationalization
