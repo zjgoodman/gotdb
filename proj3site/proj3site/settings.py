54cc2e4aac190ac52f8aa35c558383023ec96923
mhauglund <mhauglund@utexas.edu>
2015-07-12 16:47:42 -0500

    more work on houses index

--- proj3site/proj3site/settings.py
+++ proj3site/proj3site/settings.py
@@ -74,18 +74,14 @@ WSGI_APPLICATION = 'proj3site.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.mysql',
-#        'NAME': 'game_of_thrones',
-#        'USER': 'zjg95',
-#        'PASSWORD': 'downing11',
-#        'HOST': '104.130.27.102',
-#        'PORT': '3306',
-#    }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'game_of_thrones',
+        'USER': 'read',
+        'PASSWORD': '',
+        'HOST': '104.130.27.102',
+        'PORT': '3306',
+    }
 }
 
 # Internationalization
