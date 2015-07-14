d80957141c28f935a7258971705fde96bee33131
Scott Stephens <stevo4932@utexas.edu>
2015-07-14 17:36:46 -0500

    added more tests

--- proj3site/populate_content/tests.py
+++ proj3site/populate_content/tests.py
@@ -18,13 +18,14 @@ class GOT_Tests (TestCase):
                         titles='Lord of Winterfell',
                         bio='Neds bio')
 
-        # region = Person.objects.create(name='The North',
-        #                        capital_name=models.Castle(self, name='whatever'),
-        #                        ruling_house=models.House(self, name='that one house'),
-        #                        ruling_lord=models.Person(self, first_name="evil guy", last_name="prob"),
-        #                        description='may the lord bless your soul')
 
-    def test_names_1(self):
+
+        self.region = Region.objects.create(name="it has castles",
+                                            capital_name=Castle.objects.create(name='whatever'),
+                                            ruling_house=House.objects.create(name='that one house'),
+                                            description='may the lord bless your soul')
+
+    def test_person_names_1(self):
         """
         Testing Person Models
         Create and store Person model
@@ -34,74 +35,17 @@ class GOT_Tests (TestCase):
         self.assertEqual(self.person.first_name, 'Eddard')
         self.assertEqual(self.person.last_name,  'Stark')
         self.assertEqual(self.person.__str__(), "Eddard Stark")
-    #
-    # def test_person_2(self):
-    #     """
-    #     Testing Person Models
-    #     Create and store Person model (move input around)
-    #     check for correct output.
-    #     """
-    #     person = Person(last_name='Stark',
-    #                            first_name='Eddard',
-    #                            status='Dead',
-    #                            titles='Lord of Winterfell',
-    #                            bio='Neds bio')
-    #
-    #     person2 = models.Person(last_name='Stark',
-    #                            first_name='Eddard',
-    #                            status='Dead',
-    #                            titles='Lord of Winterfell',
-    #                            bio='Neds bio')
-    #
-    #     first_name = person.first_name
-    #     last_name = person.last_name
-    #     titles = person.titles
-    #     status = person.status
-    #     bio = person.bio
-    #
-    #     self.assertEqual(first_name, 'Eddard')
-    #     self.assertEqual(last_name,  'Stark')
-    #     self.assertEqual(titles,     'Lord of Winterfell')
-    #     self.assertEqual(status, 	 'Dead')
-    #     self.assertEqual(bio,        'Neds bio')
-    #     self.assertEqual(person, person2)
-    #
-    # def test_person_3(self):
-    #     """
-    #     Testing Person Models
-    #     Get the contents of the __str__ method
-    #     check for correct output (persons name).
-    #     """
-    #     person = models.Person(first_name='Eddard',
-    #                            last_name='Stark',
-    #                            titles='Lord of Winterfell',
-    #                            status='Dead',
-    #                            bio='Neds bio')
-    #
-    #     self.assertEqual(str(person), "Eddard Stark")
-    #
-    # def test_region_1(self):
-    #     """
-    #     Testing region Models
-    #     Create and store Region model
-    #     check for correct output.
-    #     """
-    #     region = models.Region(name='The North',
-    #                            capital_name=models.Castle(self, name='whatever'),
-    #                            ruling_house=models.House(self, name='that one house'),
-    #                            ruling_lord=models.Person(self, first_name="evil guy", last_name="prob"),
-    #                            description='may the lord bless your soul')
-    #     name = region.name
-    #     capital = region.capital_name
-    #     house = region.ruling_house
-    #     lord = region.ruling_lord
-    #     bio = region.description
-    #
-    #     self.assertEqual(name, 'The North')
-    #     self.assertEqual(capital,  models.Castle(self, name='whatever'))
-    #     self.assertEqual(house,     models.House(self, name='that one house'))
-    #     self.assertEqual(lord, 	 models.Person(self, first_name="evil guy", last_name="prob"))
-    #     self.assertEqual(bio,        'may the lord bless your soul')
+
+    def test_region_names_1(self):
+        """
+        Testing region Models
+        Create and store Region model
+        check for correct output.
+        """
+        self.assertEqual(self.region.__str__(), 'it has castles')
+        self.assertEqual(self.region.capital_name.__str__(), 'whatever')
+        self.assertEqual(self.region.ruling_house.__str__(), 'that one house')
+        self.assertEqual(self.region.description, 'may the lord bless your soul')
     #
     # def test_region_2(self):
     #     """
