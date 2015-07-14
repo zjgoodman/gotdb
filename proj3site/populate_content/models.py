ddde4d7e8aaa636d7bc1ef1f3435d886c344ec7c
Zachary Goodman <zjgoodman@utexas.edu>
2015-07-14 17:16:18 -0500

    updated models

--- proj3site/populate_content/models.py
+++ proj3site/populate_content/models.py
@@ -21,6 +21,10 @@ class Person(models.Model) :
     # houses associated with
     houses         = models.ManyToManyField('House', blank=True, related_name='person_houses')
     loyal_to       = models.ManyToManyField('House', blank=True, related_name='person_houses_loyal_to')
+
+    # castles that this person controls
+    castles_controlled = models.ManyToManyField('Castle', blank=True, related_name='person_castles_owned')
+    former_castles_controlled = models.ManyToManyField('Castle', blank=True, related_name='person_former_castles_owned')
     
     region_from    = models.ForeignKey('Region', null=True)
 
