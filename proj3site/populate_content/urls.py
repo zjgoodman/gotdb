1791f444dfd25b8f88274b08a9d66a4f9c96c6ae
Zachary Goodman <zjgoodman@utexas.edu>
2015-07-19 14:52:19 -0500

    updated search. #65

--- proj3site/populate_content/urls.py
+++ proj3site/populate_content/urls.py
@@ -7,6 +7,7 @@ admin.autodiscover()
 
 urlpatterns = [
     url(r'^search/', include('haystack.urls')),
+    
     url(r'^$', views.index, name='splash'),
     url(r'^about/$', views.about_index, name='about_index'),
     url(r'^about/unit_tests/$', views.unit_tests, name='unit_tests'),
